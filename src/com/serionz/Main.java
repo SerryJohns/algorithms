2df56797c3ade5c9ba792d86e810b761f79125af
John Seremba <johnseremba@Johns-MacBook-Pro.local>
2018-10-23 17:09:13 +0300

    Binary search implementation

--- src/com/serionz/Main.java
+++ src/com/serionz/Main.java
@@ -1,8 +1,16 @@
 package com.serionz;
 
+import com.serionz.searching.BinarySearch;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	/*
+	* Test binary Search
+	* NB: Binary requires a sorted array
+	 */
+        int[] list = {1, 2, 5, 7, 8, 9, 12, 14};
+        boolean status = BinarySearch.search(list, 9);
+        System.out.println("Search Result: " + status);
     }
 }
